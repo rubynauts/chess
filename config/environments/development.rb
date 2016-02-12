e67d8d7936846ed404bd4b7886558a4133bf8b90
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-02-12 04:26:09 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,9 @@ Rubynauts::Application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  #devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3030 }
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
