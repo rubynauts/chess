6cb7632257e913d63abf7ea85aa78a90658eb47b
grandtheft-spaceship <justdizon71@gmail.com>
2016-02-15 20:31:22 +0000

    Ran the migrations for games/players/pieces

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212042328) do
+ActiveRecord::Schema.define(version: 20160215095631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chess_pieces", force: true do |t|
+    t.string   "name"
+    t.string   "color"
+    t.integer  "poisition_x"
+    t.integer  "position_y"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "games", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", force: true do |t|
+    t.integer  "game_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
