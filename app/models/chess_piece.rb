808f4693c75e6816aadc7412be00909cc6d6bc2d
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-02-19 23:56:56 +0000

    added logic for horiztonal, vertical, and diagonal

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -2,6 +2,19 @@ class ChessPiece < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
 
+
+  def horiztonal_move?(endpoint_x, endpoint_y)
+    position_x != endpoint_x
+  end
+
+  def vertical_move?(endpoint_x, endpoint_y)
+    position_y != endpoint_y
+  end
+
+  def diagonal_move?(endpoint_x, endpoint_y)
+    (position_x != endpoint_x) && (position_y != endpoint_y)
+  end
+
   def is_obstructed?(endpoint_x, endpoint_y)
     if horizontal_move?(endpoint_x, endpoint_y)
       return horizontally_obstructed?(endpoint_x, endpoint_y)
