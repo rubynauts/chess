9001fd895b7ba041b59966a47dd6942bd940811d
Jackie Mundrawala <jmundrawala@gmail.com>
2016-02-19 01:40:10 +0000

    Set up STI on Pieces table

---
+++ app/models/pawn.rb
@@ -0,0 +1,3 @@
+class Pawn < ChessPiece
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216161933) do
+ActiveRecord::Schema.define(version: 20160219005841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160216161933) do
     t.integer  "position_y"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   create_table "games", force: true do |t|
