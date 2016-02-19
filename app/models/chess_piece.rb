304bdcf48f03bac7ffdd06499a894c269fd597af
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-02-19 04:20:13 +0000

    is_obstructed?

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,4 +1,17 @@
 class ChessPiece < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
+
+  def is_obstructed?(endpoint_x, endpoint_y)
+    if horizontal_move?(endpoint_x, endpoint_y)
+      return horizontally_obstructed?(endpoint_x, endpoint_y)
+    elsif vertical_move?(endpoint_x, endpoint_y)
+      return vertically_obstructed?(endpoint_x, endpoint_y)
+    elsif diagonally_obstructed?(endpoint_x, endpoint_y)
+      return diagonally_obstructed?(endpoint_x, endpoint_y)
+    else
+      return 'Invalid'
+    end
+  end
+
 end
