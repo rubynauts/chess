d99cccf96f81d53d7945dbf87b7bc1f1cf327af8
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-02-20 00:40:41 +0000

    error when testing chess piece

---
+++ spec/models/piece_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Piece do
+  describe "#horizontal_move?" do
+     context "given a piece at [4, 4]" do
+       let(:piece) {Piece.create(position_x: 4, position_y: 4)}
+       it "will return true when trying to move to [5,4]" do
+        expect(piece.horizontal_move?(5, 4)).to be_truthy
+       end
+     end
+   end
+end
