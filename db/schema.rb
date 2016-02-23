f0d676310153a35ab3e22783dcc7c59877752c6e
Jackie Mundrawala <jmundrawala@gmail.com>
2016-02-23 02:12:12 +0000

    remove name column from chess pieces table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219005841) do
+ActiveRecord::Schema.define(version: 20160223021056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chess_pieces", force: true do |t|
-    t.string   "name"
     t.string   "color"
     t.integer  "game_id"
     t.integer  "user_id"
