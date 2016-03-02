cc5c27e378099415f6e1b48d7dd608e8a8409c18
vagrant <vagrant@precise32.(none)>
2016-03-02 03:41:43 +0000

    Merging updates

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,8 @@ class Game < ActiveRecord::Base
   has_many :matches
   has_many :users, through: :matches
   has_many :chess_pieces
-
-<<<<<<< HEAD
-=======
-
+  
   def piece_in_square?(x,y)
     chess_pieces.find_by(position_x: x, position_y: y)
   end
->>>>>>> f290ac591ea1ad9178430fdeeadd572c7fdb087a
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,7 @@ ActiveRecord::Schema.define(version: 20160223021056) do
   enable_extension "plpgsql"
 
   create_table "chess_pieces", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "color"
-=======
     t.string   "color",      limit: 255
->>>>>>> f290ac591ea1ad9178430fdeeadd572c7fdb087a
     t.integer  "game_id"
     t.integer  "user_id"
     t.integer  "position_x"
@@ -32,11 +28,7 @@ ActiveRecord::Schema.define(version: 20160223021056) do
   end
 
   create_table "games", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string   "name"
-=======
     t.string   "name",       limit: 255
->>>>>>> f290ac591ea1ad9178430fdeeadd572c7fdb087a
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -49,15 +41,12 @@ ActiveRecord::Schema.define(version: 20160223021056) do
   end
 
   create_table "users", force: :cascade do |t|
-<<<<<<< HEAD
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
-=======
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
->>>>>>> f290ac591ea1ad9178430fdeeadd572c7fdb087a
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                      default: 0,  null: false
