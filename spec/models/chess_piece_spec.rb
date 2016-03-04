cb8ca9a931272407cdce97248052ee7ae1ee3259
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-03-04 00:50:34 +0000

    added tests for negative vertical

--- spec/models/chess_piece_spec.rb
+++ spec/models/chess_piece_spec.rb
@@ -69,6 +69,22 @@ RSpec.describe ChessPiece do
     end
   end
 
+  describe "#is_obstructed?" do
+    context "given a piece at [4, 4]" do
+      let(:game) {Game.create}
+      let(:piece) {ChessPiece.create(position_x: 4, position_y: 4, game: game)}
+      let(:second_piece) {ChessPiece.create(position_x: 6, position_y: 4, game: game)}
+      it "will return true when trying to move to [7,4]" do
+        second_piece
+        expect(piece.is_obstructed?(7,4)).to be_truthy
+      end
+      it "will return false when trying to move to [5,4]" do
+        second_piece
+        expect(piece.is_obstructed?(2,4)).to be_falsey
+      end
+    end
+  end
+
   
 
 end
