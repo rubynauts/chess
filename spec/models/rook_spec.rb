174918acf55daea0a9324448817259f96ce6000d
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-03-06 21:34:59 +0000

    added spec for obstruction and fixed typo

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe Rook do
     context "given a Rook at [0,0]" do
       let(:game) {Game.create}
       let(:rook) {Rook.create(position_x: 0, position_y: 0, game: game)}
-      it "will return true when trying to move to [0,6]" do
+      let(:piece) {ChessPiece.create(position_x: 4, position_y: 0, game: game)}
+      it "will return false when trying to move to [0,6]" do
         expect(rook.valid_move?(0,6)).to be_falsey
       end
       it "will return false when trying to move to [4,5]" do
         expect(rook.valid_move?(4,5)).to be_falsey
+      end
+      it "will return true when trying to move to [6,0]" do
+        piece
+        expect(rook.valid_move?(6,0)).to be_truthy
       end
     end
   end
