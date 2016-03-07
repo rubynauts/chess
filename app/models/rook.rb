2dfa88e758af27f5837edefd6fc984711f320fa5
Bijoy Varghese <bijoy.varghese.1990@gmail.com>
2016-03-07 04:45:46 +0000

    removed is_obstructed

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -3,8 +3,9 @@ class Rook < ChessPiece
   def valid_move?(endpoint_x, endpoint_y)
     if diagonal_move?(endpoint_x, endpoint_y)
       return false
+    else
+      return true
     end
-    is_obstructed?(endpoint_x, endpoint_y)
   end
 
 end
