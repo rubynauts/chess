253207611a656d884b01692a5dd6bebbdbb0fc47
harrisonlim <harrison.lim.hx@gmail.com>
2016-03-08 02:23:28 +0000

    ran bin/rake db:migrate RAILS_ENV=test command

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219005841) do
+ActiveRecord::Schema.define(version: 20160223021056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "chess_pieces", force: :cascade do |t|
-    t.string   "color",      limit: 255
+    t.string   "color"
     t.integer  "game_id"
     t.integer  "user_id"
     t.integer  "position_x"
@@ -25,11 +25,10 @@ ActiveRecord::Schema.define(version: 20160219005841) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
-    t.string   "name"
   end
 
   create_table "games", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -42,18 +41,18 @@ ActiveRecord::Schema.define(version: 20160219005841) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
