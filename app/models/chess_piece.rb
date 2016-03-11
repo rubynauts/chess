1366eff19a16404c41232298179ea1609050fa2a
grandtheft-spaceship <justdizon71@gmail.com>
2016-03-11 19:02:37 +0000

    Refactored capture logic

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -84,17 +84,13 @@ class ChessPiece < ActiveRecord::Base
     end
   end
 
-  def capture?(other_piece)
-    if chess_piece.valid_move? == true
-      if chess_piece.legal_move? == true
-        if chess_piece.is_obstructed == false
-          if chess_piece.color != other_piece.color
-            game.piece_in_square?(endpoint_x,endpoint_y) == chess_piece
-          else
-            return "Invalid Move"
-          end
-        end
-      end
+  def capture?(endpoint_x, endpoint_y)
+    other_piece = game.piece_in_square?(endpoint_x, endpoint_y)
+
+    if chess_piece.color != other_piece.color
+      return true
+    else
+      return false
     end
   end
 
