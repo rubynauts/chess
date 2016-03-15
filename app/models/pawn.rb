f01aebf5613d8e979743557a2ab4404f4bcbe58a
Jackie Mundrawala <jmundrawala@gmail.com>
2016-03-15 01:10:02 +0000

    Fix logic to account for other side of board

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -2,10 +2,14 @@ class Pawn < ChessPiece
 
 	def valid_move?(endpoint_x, endpoint_y)
 		if vertical_move?(endpoint_x, endpoint_y)
-			return true if position_y == 1 && endpoint_y <= 3
-			return true if endpoint_y == position_y + 1
+			if self.color == "White"
+				return true if position_y == 1 && endpoint_y <= 3 || endpoint_y == position_y + 1
+			else
+				return true if position_y == 6 && endpoint_y >= 4 || endpoint_y == position_y - 1
+			end
 		else
 			return false
 		end
 	end
+
 end
--- spec/models/pawn_spec.rb
+++ spec/models/pawn_spec.rb
@@ -1,12 +1,27 @@
 RSpec.describe Pawn do
 	describe "#valid_move?" do
-		context "given piece at [0,1]" do 
-			let(:pawn) {Pawn.create(position_x: 0 , position_y: 1)}
+		context "given white piece at [0,1]" do 
+			let(:pawn) {Pawn.create(position_x: 0 , position_y: 1, color: 'White')}
 			it "will return true when trying to move to [0,3]" do
 				expect(pawn.valid_move?(0,3)).to be_truthy
 			end
 			it "will return false when trying to move to [0,5]" do
 				expect(pawn.valid_move?(0,5)).to be_falsey
+			end
+			it "will return false when trying to move to [1,1]" do
+				expect(pawn.valid_move?(1,1)).to be_falsey
+			end
+		end
+	end
+
+	describe "#valid_move?" do
+		context "given black piece at [3,6]" do 
+			let(:pawn) {Pawn.create(position_x: 3 , position_y: 6, color: 'Black')}
+			it "will return true when trying to move to [3,4]" do
+				expect(pawn.valid_move?(3,4)).to be_truthy
+			end
+			it "will return false when trying to move to [3,2]" do
+				expect(pawn.valid_move?(3,2)).to be_falsey
 			end
 			it "will return false when trying to move to [1,1]" do
 				expect(pawn.valid_move?(1,1)).to be_falsey
