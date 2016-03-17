d776655b2a8d87378327fdbf79dbef6026ad213f
Jackie Mundrawala <jmundrawala@gmail.com>
2016-03-17 17:47:59 +0000

    refactored code

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -3,9 +3,9 @@ class Pawn < ChessPiece
 	def valid_move?(endpoint_x, endpoint_y)
 		if vertical_move?(endpoint_x, endpoint_y)
 			if self.color == "White"
-				return true if position_y == 1 && endpoint_y <= 3 || endpoint_y == position_y + 1
+				return position_y == 1 && endpoint_y <= 3 || endpoint_y == position_y + 1
 			else
-				return true if position_y == 6 && endpoint_y >= 4 || endpoint_y == position_y - 1
+				return position_y == 6 && endpoint_y >= 4 || endpoint_y == position_y - 1
 			end
 		else
 			return false
