8ba7a2afdeab5652f6a3de3af27d6c5c6c1f838e
harrisonlim <harrison.lim.hx@gmail.com>
2016-03-22 15:32:16 +0000

    added routes to hook up games controller to chess_piece

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rubynauts::Application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
-  
-  resources :games
+
+  resources :games do
+    member do
+      resources :chess_piece, controller: :games do
+        post :chosen_piece
+      end
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
